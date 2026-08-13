3387bf5c2a65901a77bf0a87041476f3ae03adae
UnnamedOrange <lycheng1215@sina.com>
2026-08-13 22:13:09 +0800

    Create an empty project

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author UnnamedOrange
+ * @brief The main entry.
+ * @version 0.1
+ * @date 2026-8-13
+ *
+ * @copyright Copyright (c) UnnamedOrange. Licensed under the MIT License.
+ * See the LICENSE file in the repository root for full license text.
+ */
+
+#include <Windows.h>
+
+int WINAPI wWinMain(HINSTANCE const hInstance, HINSTANCE const, PWSTR const pCmdLine, int const nCmdShow) {
+    UNREFERENCED_PARAMETER(hInstance);
+    UNREFERENCED_PARAMETER(pCmdLine);
+    UNREFERENCED_PARAMETER(nCmdShow);
+    return 0;
+}
